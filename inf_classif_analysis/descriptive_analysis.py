5c4b2edbab95247a82daafe73b4722a6a4eed68a
Marco A <47222941+Marco-Ametrano@users.noreply.github.com>
2022-01-10 22:05:22 +0100

    Create descriptive_analysis.py

---
+++ inf_classif_analysis/descriptive_analysis.py
@@ -0,0 +1,8 @@
+#AFTER PREPROCESSING AND TARGETS DEFINITION
+newdataset.describe()
+LET_IS.value_counts()
+LET_IS.value_counts().plot(kind='bar', color='c')
+Y_unica.value_counts()
+Y_unica.value_counts().plot(kind='bar', color='c')
+ZSN.value_counts().plot(kind='bar', color='c')
+Survive.value_counts().plot(kind='bar', color='c')
